69b1b54ef9e2d81060d45d0bad6cf8d4749f33fc
Roman Filippov <filiroman.tsu@gmail.com>
2015-10-28 00:41:18 +0200

    test version of bootstrap integration + port fixes

--- mcccalendar/app.js
+++ mcccalendar/app.js
@@ -17,6 +17,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', 8080);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/calendars', calendars);
 app.use('/users', users);
+app.listen(app.get('port'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
